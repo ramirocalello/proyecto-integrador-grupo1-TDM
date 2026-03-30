38506ac7e0b9af1cd56d3b913d1dd0fd8272c724
scorti-dh <scortimaderna@udesa.edu.ar>
2026-03-30 15:53:20 -0300

    Instale React Router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,24 @@
-function Header () {}
+import ElementoNav from "../ElementoNav/ElementoNav"
+
+let attrElementos = [{name:'ADMIN'}, {name:'Pages'}, {name:'Charts'}, {name:'Tables'}]
+
+function Header(props) {
+    return (
+        <nav>
+            <ul className="main-nav">
+                {attrElementos.map((e, idx) => (
+                <ElementoNav 
+                key={idx}
+                elemento = {e.name}
+                />
+            ))}
+            </ul>
+            <ul className="user">
+                <li>Nombre usuario <img src="./img/user.jpg" alt="" /></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
+
