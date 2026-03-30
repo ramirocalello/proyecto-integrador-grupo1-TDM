910eac9c10bd54d37ea901e6fef10c0b9eea317d
scorti-dh <scortimaderna@udesa.edu.ar>
2026-03-30 15:43:30 -0300

    Hice el footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,11 @@
-function Footer () {}
+function Footer() {
+    return (
+        <footer>
+            <p>Santos Corti</p>
+            <p>Ramiro Calello</p>
+            <p>Integrante 3</p>
+        </footer>
+    )
+}
+
+export default Footer
