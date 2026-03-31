f1d780e22ba48a33ebb4b908b57b5aa979858326
ramirocalello <rcalello@udesa.edu.ar>
2026-03-31 11:19:01 -0300

    mis cambios

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react"
 import Footer from "./components/Footer/Footer.js"
 import Header from "./components/Header/Header.js"
 
+
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      
+
       <Footer />
     </React.Fragment>
   );
--- src/components/Crear/Crear.js
+++ src/components/Crear/Crear.js
@@ -1,1 +1,47 @@
-function Create () {}
+import {Component} from React
+
+class Crear extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            email: ""
+            password:""
+            cookie: ""
+        }
+
+        evitarSubmit = (event)=>{
+            event.preventDefault();
+        }
+
+        controlarCambios = (event) => {
+            this.setState({valor:event.target.value})
+        }
+
+        render(){
+            return(
+                <div>
+                    <h2>Registrarse</h2>
+                    <form onSubmit={(event)=> this.evitarSubmit(event)}>
+                        <label>Name or email:</label>
+                        <input type= "email" name="email" onChange={(event)=>this.controlarCambios(event)} value={this.state.email}></input>
+                        <label>Password:</label>
+                        <input type = "password" name = "password"  onChange={(event)=>this.controlarCambios(event)} value={this.state.password}></input>
+                        <input type = "submit" value = "Registrarse"></input>
+
+                    </form>
+                </div>
+            )
+        }
+    }
+}
+
+
+
+        
+        
+        
+        
+        
+    
+
+Export default Crear
