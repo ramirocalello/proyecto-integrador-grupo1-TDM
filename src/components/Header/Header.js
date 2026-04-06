5629c144881917e9208baefca10ab91b52c6a1c5
Fwarszawiak <fwarszawiakgostanian@udesa.edu.ar>
2026-04-06 17:25:52 -0300

    empeze header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,27 @@
 import { Component } from "react";
 import Elementosnav from "../Elementosnav/Elementosnav"
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: [{ name: "home", path: "/" }, { name: "Crea tu cuenta", path: "/crear" }, { name: "favoritos", path: "/favoritos" }],
-      invisiible: [{ name: "crear", path: "/crear" }, { name: "login", path: "/login" }],
-      cookie: "",
-    }
-  }
-  render() {
-    return (
-      <section className="card-container">
-        {this.state.cookie !== '' ? (
-          this.state.visible.map((e) => (
-            <Elementosnav
-              name={e.name}
-              path={e.path}
-            />
-          ))
-        ) : (
-          this.state.invisiible.map((e) => (
-            <Elementosnav
-              name={e.name}
-              path={e.path} />
-          ))
-        )
-        }
-      </section>
-    )
-  }
-}
 
-export default Header
+class Header extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     permanente: [{ name: "Home", path: "/" }],
+     condicionales: [{ name: "Login", path: "/login" }, { name: "Crea tu cuenta", path: "/crear" }, { name: "Favoritos", path: "/favoritos" }],
+     cookie: "",
+   }
+ }
+ render() {
+   return (
+     <nav>
+       <ul className="nav nav-tabs my-4">
+         <Elementosnav
+           name={this.state.permanente[0].name}
+           path={this.state.permanente[0].path}
+         />
+
+
+export default Header;
+
+
+
