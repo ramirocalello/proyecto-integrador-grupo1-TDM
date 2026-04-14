60aa858b07787ef8bdfe331b22d83bae33c651a3
scorti-dh <scortimaderna@udesa.edu.ar>
2026-04-14 09:40:42 -0300

    Cambie el header para debuggear las cookies

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
             <Link to="/series" className="nav-link">Series</Link>
           </li>
 
-          {![this.state.cookie] ? (
+          {/* {![this.state.cookie] ? ( */}
             <div className="condicional">
               < li className="nav-item">
                 <Link to="/register" className="nav-link">Crear Cuenta</Link>
@@ -35,12 +35,12 @@ class Header extends Component {
                 <Link to="/login" className="nav-link">Iniciar Sesion</Link>
               </li>
             </div>
-          ) : (
+          {/* ) : ( */}
             <li className="nav-item">
               <Link to="/favoritos" className="nav-link">Favoritos</Link>
             </li>
-          )
-          }
+          {/* )
+          } */}
 
         </ul>
       </nav >
