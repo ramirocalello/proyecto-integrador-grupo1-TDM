63ce263ebe0e4fc404d0298f660d3c0015eb3592
scorti-dh <scortimaderna@udesa.edu.ar>
2026-04-20 15:13:02 -0300

    Arregle que el boton de favoritos de las cards solo aparezca cuando existe una cookie

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,76 +1,76 @@
-import { Link } from 'react-router-dom/cjs/react-router-dom.min.js'
-import { Component } from "react";
-import './Card.css'
-import Cookies from 'universal-cookie'
-const cookies = new Cookies()
+    import { Link } from 'react-router-dom/cjs/react-router-dom.min.js'
+    import { Component } from "react";
+    import './Card.css'
+    import Cookies from 'universal-cookie'
+    const cookies = new Cookies()
 
-class Card extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            className: 'hidden-p',
-            texto: 'Ver Descpricíon',
-            cookie: cookies.getAll()
+    class Card extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                className: 'hidden-p',
+                texto: 'Ver Descpricíon',
+                cookie: cookies.get('usuario')
+            }
         }
+
+        descipcion(e) {
+            e.preventDefault()
+            this.setState({
+                className: this.state.className === "hidden-p" ? "visible-p" : "hidden-p",
+                texto: this.state.texto === "Ver Descpricíon" ? "Minimizar " : "Ver Descpricíon"
+            })
+        }
+
+    favoritos(e) {
+        let favoritos = localStorage.getItem('favoritos')
+
+        if (favoritos !== null) {
+            favoritos = JSON.parse(favoritos)
+        } else {
+            favoritos = []
+        }
+
+        favoritos.push(this.props.id)
+
+        localStorage.setItem('favoritos', JSON.stringify(favoritos))
     }
-
-    descipcion(e) {
-        e.preventDefault()
-        this.setState({
-            className: this.state.className === "hidden-p" ? "visible-p" : "hidden-p",
-            texto: this.state.texto === "Ver Descpricíon" ? "Minimizar " : "Ver Descpricíon"
-        })
-    }
-
- favoritos(e) {
-    let favoritos = localStorage.getItem('favoritos')
-
-    if (favoritos !== null) {
-        favoritos = JSON.parse(favoritos)
-    } else {
-        favoritos = []
-    }
-
-    favoritos.push(this.props.id)
-
-    localStorage.setItem('favoritos', JSON.stringify(favoritos))
-}
-    render() {
-        
-        return (
-            <article className="single-card-movie">
-                <img src={`https://image.tmdb.org/t/p/w342${this.props.img}`} className="card-img-top"
-                    alt="..." />
-                <div className="cardBody">
-                    <h5 className="card-title">{this.props.title}</h5>
-                    <p className={`card-text ${this.state.className}`}>{this.props.desc}</p>
-                    <div className="link-card">
-                        <button className="btn btn-primary" onClick={(e)=> this.descipcion(e)}>
-                            {this.state.texto}
-                        </button>
-
-                        <Link to={`/detalle/${this.props.id}`} className="btn btn-primary">
-                            Ir al detalle
-                        </Link>
-
-                        {cookies.get("user") === undefined ? (
-                            <button 
-                                onClick={(e) => this.favoritos(e)} 
-                                className={`btn alert-primary ${this.state.favoritosDisplay}`}
-                            >
-                                🩶
+        render() {
+            
+            return (
+                <article className="single-card-movie">
+                    <img src={`https://image.tmdb.org/t/p/w342${this.props.img}`} className="card-img-top"
+                        alt="..." />
+                    <div className="cardBody">
+                        <h5 className="card-title">{this.props.title}</h5>
+                        <p className={`card-text ${this.state.className}`}>{this.props.desc}</p>
+                        <div className="link-card">
+                            <button className="btn btn-primary" onClick={(e)=> this.descipcion(e)}>
+                                {this.state.texto}
                             </button>
-                        ) : (
-                            
-                            <button className="hidden-btn"></button>
-                        )
-                        }
-                    
-                    </div>
-                </div>
-            </article>
-        );
-    };
-}
 
-export default Card
+                            <Link to={`/detalle/${this.props.id}`} className="btn btn-primary">
+                                Ir al detalle
+                            </Link>
+
+                            {this.state.cookie !== undefined ? (
+                                <button 
+                                    onClick={(e) => this.favoritos(e)} 
+                                    className={`btn alert-primary ${this.state.favoritosDisplay}`}
+                                >
+                                    🩶
+                                </button>
+                            ) : (
+                                
+                                <button className="hidden-btn"></button>
+                            )
+                            }
+                        
+                        </div>
+                    </div>
+                </article>
+            );
+        };
+    }
+
+    export default Card
